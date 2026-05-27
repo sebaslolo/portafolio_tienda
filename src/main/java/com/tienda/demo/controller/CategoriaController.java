b6d1150bfc164d41c8da34c02bfc2bb8687688de
sebaslolo <sebaslopez2009@gmail.com>
2026-05-27 00:43:43 -0600

    Commit 3 Semana 3 Error Con css

---
+++ src/main/java/com/tienda/demo/controller/CategoriaController.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.tienda.demo.controller;
+
+ 
+import com.tienda.demo.service.CategoriaService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+ 
+@Controller
+@RequestMapping("/categoria")
+public class CategoriaController {
+ 
+    // El servicio es final para asegurar la inmutabilidad
+    private final CategoriaService categoriaService;
+ 
+    // Inyección por constructor (No requiere @Autowired en Spring moderno)
+    public CategoriaController(CategoriaService categoriaService) {
+        this.categoriaService = categoriaService;
+    }
+ 
+    @GetMapping("/listado")
+    public String inicio(Model model) {
+        var categorias = categoriaService.getCategorias(false);
+        model.addAttribute("categorias", categorias);
+        model.addAttribute("totalCategorias", categorias.size());
+        return "/categoria/listado";
+    }
+}
