9da3fc2df1c8fdb85dae066b5ad3f69be2fd1752
sebaslolo <sebaslopez2009@gmail.com>
2026-07-01 15:08:24 -0600

    Commit 7 Semana 8 consultas

--- src/main/java/com/tienda/demo/repository/ProductoRepository.java
+++ src/main/java/com/tienda/demo/repository/ProductoRepository.java
@@ -1,22 +1,29 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.tienda.demo.repository;
-
-/*
- *
- * @author allam
- */
 
 import com.tienda.demo.domain.Producto;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-@Repository
 public interface ProductoRepository extends JpaRepository<Producto, Integer> {
 
     public List<Producto> findByActivoTrue();
-    
+
+    //Consulta derivada que recupera los producto de un rango de precio y los ordena por precio ascendentemente
+    public List<Producto> findByPrecioBetweenOrderByPrecioAsc(double precioInf, double precioSup);
+
+   // Consulta JPQL
+@Query(value = "SELECT p FROM Producto p WHERE p.precio BETWEEN :precioInf AND :precioSup ORDER BY p.precio ASC")
+public List<Producto> consultaJPQL(
+        @Param("precioInf") double precioInf,
+        @Param("precioSup") double precioSup);
+
+
+// Consulta SQL nativa
+@Query(nativeQuery = true,
+        value = "SELECT * FROM producto p WHERE p.precio BETWEEN :precioInf AND :precioSup ORDER BY p.precio ASC")
+public List<Producto> consultaSQL(
+        @Param("precioInf") double precioInf,
+        @Param("precioSup") double precioSup);
 }
